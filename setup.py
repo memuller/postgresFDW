04b83d1232a72c71315a9178d1ff5f6cb1f969d9
Matheus E. Muller <hello@memuller.com>
2018-07-03 14:36:15 -0300

    Adds setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+  name = 'postgres-wikipedia',
+  version = '0.0.1',
+  author = 'Matheus E. Muller',
+  author_email = 'hello@memuller.com',
+  license = 'UNLICENSE',
+  packages = ['postgres-wikipedia']
+)
