888c2cc3e300135a0fec2054282f726abc1f7323
Matheus E. Muller <hello@memuller.com>
2018-07-03 14:36:31 -0300

    Adds simple testing FDW

---
+++ postgres-wikipedia/postgres-wikipedia.py
@@ -0,0 +1,19 @@
+from multicorn import ForeignDataWrapper
+import requests
+
+class ConstantForeignDataWrapper(ForeignDataWrapper):
+
+  def __init__(self, options, columns):
+    # options: dict
+    # columns: [ column_name: type ]
+    super(ConstantForeignDataWrapper, self).__init__(options, columns)
+    self.columns = columns
+  
+  def execute(self, quals, columns):
+    # @ iteratable [ in order] || [ column_name: value ]
+    for i in range(20):
+      line = {}
+      for column in self.columns:
+        line[column] = '%s: %s' % (column, i)
+      yield line
+  
