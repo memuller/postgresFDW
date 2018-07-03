fe472a2c5b89f9f898f483affaf8a3cd85c37ee5
Matheus E. Muller <hello@memuller.com>
2018-07-03 14:54:12 -0300

    Moves module around

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages, Extension
 
 setup(
-  name = 'postgres-wikipedia',
+  name = 'memullerPostgresFDW',
   version = '0.0.1',
   author = 'Matheus E. Muller',
   author_email = 'hello@memuller.com',
   license = 'UNLICENSE',
-  packages = ['postgres-wikipedia']
+  packages = ['lib']
 )
